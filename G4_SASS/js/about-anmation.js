a13b96cb3bcf714fa36621c51edbe9b900060feb
HSU YU WEI <33489732+ninewill@users.noreply.github.com>
2018-08-13 22:20:15 +0800

    新增動畫及關於文字
    1.新增運動中心場地及服務動畫
    2.新增關於我們文字簡介

---
+++ G4_SASS/js/about-anmation.js
@@ -0,0 +1,39 @@
+$("#animation-crane").hover(
+  function() {
+    $(this).addClass("activate"),
+      $(".an-crane-1-wrap, .an-crane-2-wrap, .an-crane-area-wrap").css(
+        "animation-play-state",
+        "running"
+      ),
+      $(".an-crane-3-wrap").css("animation-play-state", "paused");
+    var e = $(".an-crane-area-2");
+    $(this)
+      .mousemove(function(t) {
+        var n = e.offset().left + e.width() / 2,
+          a = e.offset().top + e.height() / 2,
+          r = parseInt((t.pageX - n) / 5),
+          i = parseInt((t.pageY - a) / 10);
+        e.css({
+          transform: "translateX(" + r + "px) translateY(" + i + "px)"
+        });
+      })
+      .mouseleave(function(t) {
+        t.preventDefault(),
+          e.css({
+            transform: "translateX(0) translateY(0)"
+          });
+      }),
+      $(this).click(function(e) {
+        e.preventDefault(), $(".an-crane-area-2").fadeOut(3e3);
+      });
+  },
+  //執行程式
+  function() {
+    $(".an-crane-area-2").fadeIn(),
+      $(".an-crane-1-wrap, .an-crane-1-wrap, .an-crane-area-wrap").css(
+        "animation-play-state",
+        "paused"
+      ),
+      $(".an-crane-3-wrap").css("animation-play-state", "running");
+  }
+);
