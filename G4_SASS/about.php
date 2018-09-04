ba7ccb32f999174cc861e855ba51c5e643a21309
will00 <wtony1239271@gmail.com>
2018-09-04 14:05:10 +0800

    關於我們rwd修正

--- G4_SASS/about.php
+++ G4_SASS/about.php
@@ -223,7 +223,7 @@
   <script src="js/about-anmation.js"></script>
   <script src="libs/scrollmagic/scrollmagic/minified/ScrollMagic.min.js"></script>
   <script src="libs/scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js"></script>
-  <script src="libs/scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js"></script>
+  <!-- <script src="libs/scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js"></script> -->
   <script src="js/about-scrollmagic.js"></script>
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
   <script src="js/map.js"></script>
