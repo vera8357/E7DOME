f3d90b81c1c3b4680d05baae563ce51dc0d80196
jeremy60317 <39216641+jeremy60317@users.noreply.github.com>
2018-09-04 23:04:22 +0800

    update bowling.php sql data

--- G4_SASS/bowling.php
+++ G4_SASS/bowling.php
@@ -195,7 +195,7 @@
                     <?php
                     
                         require_once("php/connect_g4.php");
-                        $sql = "SELECT * FROM team JOIN booking ON (team.BOO_NO = booking.BOO_NO) JOIN facility ON (facility.FAC_NO = booking.FAC_NO) where facility.CATE_NO = 2 order by facility.FAC_NO desc limit 3";
+                        $sql = "SELECT * FROM team JOIN booking ON (team.BOO_NO = booking.BOO_NO) JOIN facility ON (facility.FAC_NO = booking.FAC_NO) where booking.BOO_DATE > CURRENT_DATE() and facility.CATE_NO = 2 order by facility.FAC_NO desc limit 3";
                         $team = $pdo->query( $sql);
                         $teams = $team->fetchAll(PDO::FETCH_ASSOC);
                         foreach($teams as $i=>$teamsRow){
