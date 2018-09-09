7a21373bcfa765e886a392c97b0f64c8ed63f1fe
dumpingmori <magicdode@gmail.com>
2018-09-10 02:35:23 +0800

    editgroup and some  fixed
    editgroup and some  fixed

--- G4_SASS/php/memBn_check.php
+++ G4_SASS/php/memBn_check.php
@@ -1,21 +1,20 @@
 <?php
                     if(!isset($_SESSION["MEM_NO"]) != "null"){
-                    $team_leader = "SELECT * FROM team WHERE team.TEAM_NO=$TEAM_NO ";
-                    $team_member = "SELECT * FROM team_mem WHERE team_mem.TEAM_NO=$TEAM_NO";
-                    $leader = $pdo->query( $team_leader );
-                    $member = $pdo->query( $team_member );
-                    $leader = $leader->fetch(PDO::FETCH_ASSOC);
-                    $member = $member->fetch(PDO::FETCH_ASSOC);
-                    $MEM_NO = $_SESSION["MEM_NO"];
-                    $mem = $member["MEM_NO"];
-                            if($MEM_NO == $mem){
+                        $MEM_NO = $_SESSION["MEM_NO"];
+                        $team_leader = "SELECT * FROM team WHERE team.TEAM_NO=$TEAM_NO ";
+                        $team_member = "SELECT * FROM team_mem WHERE team_mem.TEAM_NO=$TEAM_NO";
+                        $leader = $pdo->query( $team_leader );
+                        $member = $pdo->query( $team_member );
+                        $leader = $leader->fetch(PDO::FETCH_ASSOC);
+                        $member = $member->fetch(PDO::FETCH_ASSOC);
+                            if($MEM_NO == ($member["MEM_NO"])){
                                 echo "<form action='php/leaveTeam.php' method='get' id='leaveForm' name='leaveForm'>";
                                 echo "<input type='hidden' name='TEAM_NO' value='$TEAM_NO'>";
                                 echo "<button class='button join-button' id='join'>我要退團";
                                 echo "</button></form>";
 
                             }elseif($MEM_NO == ($leader["MEM_NO"])){
-                                echo "<a href='membergroup.php'>";
+                                echo "<a href='php/editgroup.php?TEAM_NO=$TEAM_NO'>";
                                 echo "<button class='button join-button' id='join'>管理揪團";
                                 echo "</button></a>";
                             }elseif($rows+1 == ($teamsRow["TEAM_MEM"])){
@@ -29,9 +28,8 @@
                                 echo "</button></form>";
                             }
                         }else{
-                            echo "<button class='button join-button' id='join'>";
-                            echo"請先登入";
-                            echo "</button></a>";
-                        }
+                            echo "<button class='button join-button' id='join'>請先登入";
+                            echo "</button>";
+                        };
 
 ?>
---
+++ G4_SASS/php/upfile_group.php
@@ -0,0 +1,53 @@
+<?php
+ob_start();
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Examples</title>
+</head>
+<body>
+
+<?php 
+
+$MEM_NO = $_GET["TEAM_NO"];
+require_once("connect_g4.php");
+switch($_FILES['upfile']['error']){
+  case UPLOAD_ERR_OK:
+    if( file_exists("../images")===false){
+    	mkdir("../images");
+    }
+    $from = $_FILES['upfile']['tmp_name'];
+    $fileto =pathinfo($_FILES['upfile']['name']);
+    $filext = $fileto['basename'];
+    $to = "../images{$filext}";
+    $filname = "{$filext}";
+    if(copy( $from, $to)){
+      $memberfile = "UPDATE member SET MEM_IMG = '{$filname}' WHERE MEM_NO = $MEM_NO";
+      $pdo->exec($memberfile);
+      $_SESSION["MEM_IMG"] = $filname;   
+
+      header('location:../grou.php');
+
+    }
+    break;
+  case UPLOAD_ERR_INI_SIZE:
+    echo "上傳檔案太大，不得超過", ini_get("upload_max_filesize"),"<br>";
+    break;
+  case UPLOAD_ERR_FORM_SIZE:
+    echo "上傳檔案太大，不得超過",  $_REQUEST["MAX_FILE_SIZE"],"<br>";
+    break;
+  case UPLOAD_ERR_PARTIAL:
+    echo "上傳失敗，請重新上傳檔案<br>";
+    break;
+  case UPLOAD_ERR_NO_FILE:
+    echo "echo 未上傳檔案太大<br>";
+}
+
+?>
+
+</body>
+</html>
