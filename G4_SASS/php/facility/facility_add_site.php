68a9e64fecb8a825024f274a2db77aa4f451deb7
jeremy60317 <39216641+jeremy60317@users.noreply.github.com>
2018-09-10 17:28:39 +0800

    update

--- G4_SASS/php/facility/facility_add_site.php
+++ G4_SASS/php/facility/facility_add_site.php
@@ -1,13 +1,14 @@
 <?php
 try{
-    require_once("php/connect_g4.php");
-    if( file_exists("images/sport//")===false){
-        mkdir("images/sport//");
+    require_once("../connect_g4.php");
+    if( file_exists("../../images/fac_img//")===false){
+        mkdir("../../images/fac_img//");
     }
     $from = $_FILES['facimg']['tmp_name'];
-    $to = "images/sport//" . $_FILES['facimg']['name'];
+    $to = "../../images/fac_img//" . $_FILES['facimg']['name'];
     if(copy( $from, $to)){
         echo "上傳成功<br>";
+        // echo "<script>alert('OK')</script>";
         $fac_name = $_REQUEST["fac_name"];
         $fac_desc = $_REQUEST["fac_desc"];
         $fac_points = $_REQUEST["fac_points"];
@@ -16,7 +17,7 @@ try{
         $cate_no = $_REQUEST["cate_no"];
         $sql = "INSERT INTO facility(FAC_NO,CATE_NO,FAC_NAME,FAC_DESC,FAC_MEM,FAC_POINTS,FAC_IMG1,FAC_STATUS)values(null,'$cate_no','$fac_name','$fac_desc','$fac_mem','$fac_points','$facimg','0')";
         $pdo->exec($sql);
-        header("location:back_fac.html");
+        header("location:../../back_fac.html");
     }
     // break;
 }catch(PDOException $e){
