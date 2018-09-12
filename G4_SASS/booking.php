fbf488f00dfdd9db9f8261e44f124865871e9ea0
peter <j3pxy6@gmail.com>
2018-09-12 18:27:07 +0800

    bower install font-awesome

--- G4_SASS/booking.php
+++ G4_SASS/booking.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>預約場地</title>
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+	<link rel="stylesheet" href="libs/components-font-awesome/css/fontawesome-all.min.css">
 	<link rel="stylesheet" href="css/booking.css">
 	<link rel="stylesheet" href="css/font.css">
 </head>
